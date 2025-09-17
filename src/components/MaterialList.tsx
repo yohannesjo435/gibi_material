3d432e511ce005497ba7c86baf6d965eb9a67df0
yohannes <yohannesgoat@gmail.com>
2025-09-17 20:38:37 -0300

    feat: made material list resposive in every screen

--- src/components/MaterialList.tsx
+++ src/components/MaterialList.tsx
@@ -18,7 +18,7 @@ import {
 } from "@/components/ui/select";
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { Eye, Grid2x2, List } from "lucide-react";
+import { Eye, Grid2x2, List, HardDriveDownload } from "lucide-react";
 
 const MaterialList = () => {
   return (
@@ -27,7 +27,7 @@ const MaterialList = () => {
         <h2 className="font-semibold">Year 4 - Computer Science Materials</h2>
 
         <div className="flex flex-row items-center gap-3">
-          <p>24 material found</p>
+          <p className="hidden md:block">24 material found</p>
 
           <Select>
             <SelectTrigger>
@@ -41,14 +41,18 @@ const MaterialList = () => {
               </SelectGroup>
             </SelectContent>
           </Select>
-          <Grid2x2 size={18} />
-          <List size={18} />
+
+          <Grid2x2 className="hidden md:block" size={18} />
+          <List className="hidden md:block" size={18} />
         </div>
       </div>
 
       {course.map((dep, index) => (
-        <Card key={index} className="cursor-pointer grid grid-cols-2">
-          <CardHeader className="flex items-center gap-3.5">
+        <Card
+          key={index}
+          className="cursor-pointer grid grid-cols-1 md:grid-cols-2"
+        >
+          <CardHeader className="flex flex-row-reverse justify-between md:justify-start md:flex-row md:items-center md:gap-3.5">
             <Image
               src={"/department_icons/is.png"}
               width={30}
@@ -60,18 +64,27 @@ const MaterialList = () => {
               <div className="flex gap-2">
                 <CardDescription>{dep.nickName}</CardDescription>
                 <CardDescription>{dep.author}</CardDescription>
-                <CardDescription>{dep.size}</CardDescription>
+                <CardDescription className="font-medium">
+                  {dep.size}mb
+                </CardDescription>
                 <CardDescription>{dep.updatedDate}</CardDescription>
               </div>
             </div>
           </CardHeader>
+
           <CardFooter className="flex justify-end">
-            <CardAction className="flex gap-2">
-              <Button variant={"outline"} className="bg-gray-200">
+            <CardAction className="w-full flex justify-end gap-2">
+              <Button
+                variant={"outline"}
+                className="w-[90%] md:w-max bg-blue-500  md:bg-gray-200"
+              >
                 <Eye />
                 Preview
               </Button>
-              <Button className="bg-blue-500">Download</Button>
+              <Button className="hidden md:block bg-blue-500 ">Download</Button>
+              <Button className="w-[10%] md:hidden ">
+                <HardDriveDownload />
+              </Button>
             </CardAction>
           </CardFooter>
         </Card>
