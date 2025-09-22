cf28acb79a028312975f7a9536dac8e6cb3fac33
yohannes <yohannesgoat@gmail.com>
2025-09-22 18:20:46 -0300

    fix: clientWrapper children data type

--- src/app/ClientLayoutWrapper.tsx
+++ src/app/ClientLayoutWrapper.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { AppProgressProvider as ProgressProvider } from "@bprogress/next";
 
-export default function ClientLayoutWrapper({ children }) {
+export default function ClientLayoutWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <ProgressProvider>{children}</ProgressProvider>;
 }
