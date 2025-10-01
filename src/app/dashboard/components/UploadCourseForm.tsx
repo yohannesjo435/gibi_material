88d51b66d7e5876d2319ed584e557f741c4da7c7
yohannes <yohannesgoat@gmail.com>
2025-10-01 16:25:12 -0300

    feat: file upload to study_materials bucket

--- src/app/dashboard/components/UploadCourseForm.tsx
+++ src/app/dashboard/components/UploadCourseForm.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { CloudUpload } from "lucide-react";
@@ -12,17 +13,42 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { uploadFile } from "@/lib/storage";
 
 const UploadCourseForm = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [url, setUrl] = useState("");
+  const bucket = "study_materials";
+
+  async function handleUpload(e: React.FormEvent) {
+    e.preventDefault();
+    const key = `${Date.now()}-${file?.name}`;
+    if (!file) return;
+    try {
+      const { url } = await uploadFile(bucket, key, file);
+      setUrl(url);
+    } catch (err) {
+      console.error("Upload failed: ", err);
+    }
+  }
   return (
-    <div className="grid">
+    <form className="grid" onSubmit={handleUpload}>
       <h3 className="text-[18px] font-semibold">Upload Course</h3>
       <h4 className="mb-7 text-gray-500 text-[13px]">
         Fill out the course and upload new materials
       </h4>
 
       <div className=" m-auto w-full h-52 border-dashed border-1 border-black dark:border-white rounded-2xl mb-5">
-        <UploadInput />
+        <div className="text-center flex flex-col justify-center items-center gap-4 w-full h-full">
+          <CloudUpload />
+          <Label>Upload</Label>
+          <Input
+            className="max-w-48"
+            type="file"
+            onChange={(e) => setFile(e.target.files?.[0] || null)}
+          />
+        </div>{" "}
       </div>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
         <div className="grid gap-4">
@@ -58,7 +84,7 @@ const UploadCourseForm = () => {
         </div>
         <Button className="py-5 bg-blue-500 cursor-pointer">Upload</Button>
       </div>
-    </div>
+    </form>
   );
 };
 
@@ -69,7 +95,7 @@ export function UploadInput() {
     <div className="text-center flex flex-col justify-center items-center gap-4 w-full h-full">
       <CloudUpload />
       <Label>Upload</Label>
-      <Input className="max-w-48" type="file"></Input>
+      <Input className="max-w-48" type="file" />
     </div>
   );
 }
