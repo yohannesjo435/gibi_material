c8d4733d5bc83f5ac668dbb6c86976d2ca5151f0
yohannes <yohannesgoat@gmail.com>
2025-10-07 08:44:05 -0300

    cleanup: unused file

--- src/app/api/download/route.ts
+++
@@ -1,9 +0,0 @@
-// import { supabase } from "@/lib/supabaseClient";
-
-// export async function GET(request: Request) {
-//   const { data, error } = await supabase.storage
-//     .from("study_materials") // ✅ your bucket name
-//     .createSignedUrl(relativePath, 60, { download: true });
-
-//   return Response.redirect(data.signedUrl, 302);
-// }
