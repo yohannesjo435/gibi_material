bbcc7417d9259c2793a9c257e894cdaa844b36e0
yohannes <yohannesgoat@gmail.com>
2025-10-14 10:16:16 -0300

    fix: updated parmas to get access using context added promise for api/departments/route

--- src/app/api/departments/[id]/route.ts
+++ src/app/api/departments/[id]/route.ts
@@ -3,9 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
-  const { id } = await params;
+  const { id } = await context.params;
   const data = await getDepartmentById(id);
 
   if (!data) {
